96dd8b1db01dad14102f406e3e68753c6dda4ff1
Daria Eremina <djerjomina@gmail.com>
2024-11-12 17:41:18 -0800

    Season change added for BG

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ const int SCREEN_HEIGHT = 600;
 
 const char* GAME_OVER_TEXT = "Game Over!";
 
-Rectangle bunny = {10, 490, 100, 100};
-Rectangle fox = {10, 10, 100, 100};
-bool gameOver = false;
+Rectangle bunny;
+Rectangle fox;
+float seasonX;
+bool gameOver;
 
 void restart()
 {
-	bunny.x = 10;
-	bunny.y = 490;
-	fox.x = 10;
-	fox.y = 10;
+	bunny = (Rectangle){10, 490, 100, 100};
+	fox = (Rectangle){10, 10, 100, 100};
+	seasonX = SCREEN_WIDTH;
 	gameOver = false;
 }
 
@@ -27,6 +27,23 @@ int main()
 {	
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bunny Game");
 	SetTargetFPS(60);
+
+	// Textures and shader
+	// At the moment textures are just white and green colors.
+	// Later we might want to change the shader so that it applies color multiplication to a single texture.
+	
+	Image imWinter = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, (Color){ 255, 255, 255, 255 });
+    Texture texWinter = LoadTextureFromImage(imWinter);
+    UnloadImage(imWinter);
+
+    Image imSummer = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, (Color){ 0, 255, 0, 255 });
+    Texture texSummer = LoadTextureFromImage(imSummer);
+    UnloadImage(imSummer);
+
+	Shader shader = LoadShader(0, "color_mix.fs");
+	// devider and texture 1 (from the shader, to be set later)
+    int texWinterLoc = GetShaderLocation(shader, "texture1");
+    int dividerLoc = GetShaderLocation(shader, "divider");
 
 	restart();
 
@@ -49,8 +66,15 @@ int main()
 
 			bunny.x = Clamp(bunny.x, 0, SCREEN_WIDTH-bunny.width);
 			bunny.y = Clamp(bunny.y, 0, SCREEN_HEIGHT-bunny.height);
-		}
 
+			// Advance the season
+			if (seasonX>0)
+			{
+				seasonX -= 1;
+			}
+			float dividerValue = Remap(seasonX, 0, SCREEN_WIDTH, 0, 1);
+			SetShaderValue(shader, dividerLoc, &dividerValue, SHADER_UNIFORM_FLOAT);			
+		}		
 
 		//Move the fox
 		// Calculate the vector from the fox to the bunny, normalize it and multiply by the fox speed
@@ -64,7 +88,8 @@ int main()
 		// Collision detection
 		if (CheckCollisionRecs(bunny, fox))
 		{
-			gameOver = true;
+			// Commented out for testing purposes
+			//gameOver = true;
 		}
 
 		// Restart
@@ -75,16 +100,27 @@ int main()
 
 		// Draw
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawRectangleRec(bunny, DARKGRAY);
-		DrawRectangleRec(fox, ORANGE);
-		if (gameOver)
-		{
-			int gameOverWidth = MeasureText(GAME_OVER_TEXT, 70);
-			int restartWidth = MeasureText("Press R to restart", 50);
-			DrawText(GAME_OVER_TEXT, SCREEN_WIDTH/2-gameOverWidth/2, SCREEN_HEIGHT/2-50, 70, RED);
-			DrawText("Press R to restart", SCREEN_WIDTH/2-restartWidth/2, SCREEN_HEIGHT/2+30, 50, RED);
-		}
+			ClearBackground(RAYWHITE);
+
+			// Draw the season
+			BeginShaderMode(shader);
+                SetShaderValueTexture(shader, texWinterLoc, texSummer);
+                DrawTexture(texWinter, 0, 0, WHITE);
+            EndShaderMode();
+
+			// Draw characters
+			DrawRectangleRec(bunny, DARKGRAY);
+			DrawRectangleRec(fox, ORANGE);
+
+			DrawLineEx((struct Vector2){seasonX,0}, (struct Vector2){seasonX,SCREEN_HEIGHT}, 4, BLACK);
+			
+			if (gameOver)
+			{
+				int gameOverWidth = MeasureText(GAME_OVER_TEXT, 70);
+				int restartWidth = MeasureText("Press R to restart", 50);
+				DrawText(GAME_OVER_TEXT, SCREEN_WIDTH/2-gameOverWidth/2, SCREEN_HEIGHT/2-50, 70, RED);
+				DrawText("Press R to restart", SCREEN_WIDTH/2-restartWidth/2, SCREEN_HEIGHT/2+30, 50, RED);
+			}
 		EndDrawing();
 	}
 	CloseWindow();
