0b9e91d5117004b900260060c887f4b08d5fe88a
Patrickgh3 <patrickjtraynor@gmail.com>
2024-12-10 18:45:47 -0800

    Add multiple foxes and stuff

--- main.c
+++ main.c
@@ -8,16 +8,15 @@ const float FOX_SPEED = 4.0f;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-const char* GAME_OVER_TEXT = "Game Over!";
-
 const float SEASON_DURATION = 100.0f;
 const float SEASON_CHANGE_SPEED = 1.0f;
-const float COLOR_CHANGE_DELAY = 30.0f;
+const float COLOR_CHANGE_DELAY = 00.0f;
 
 Rectangle bunny;
-Rectangle fox;
-Rectangle trap;
 Rectangle lake;
+int numFoxes;
+Rectangle foxes[5];
+Rectangle traps[3];
 bool gameOver;
 bool win;
 int score;
@@ -26,39 +25,65 @@ int counter;
 bool isWinter;
 bool isBunnyWinter;
 
-bool areTrapsInYear;
-bool areLakesInYear;
-
 int year;
-Rectangle traps[3];
 int health;
 
-void respawnFox() {
+void respawnFox(int index) {
+    Rectangle * fox = &foxes[index];
     if (GetRandomValue(0, 1) == 0)
     {
         // Top and bottom
-        fox.x = GetRandomValue(-fox.width, SCREEN_WIDTH);
-        fox.y = GetRandomValue(0, 1) ? -fox.height : SCREEN_HEIGHT;
+        fox->x = GetRandomValue(-fox->width, SCREEN_WIDTH);
+        fox->y = GetRandomValue(0, 1) ? -fox->height : SCREEN_HEIGHT;
     }
     else
     {
         // Left and right
-        fox.y = GetRandomValue(-fox.height, SCREEN_HEIGHT);
-        fox.x = GetRandomValue(0, 1) ? -fox.width : SCREEN_WIDTH;
+        fox->y = GetRandomValue(-fox->height, SCREEN_HEIGHT);
+        fox->x = GetRandomValue(0, 1) ? -fox->width : SCREEN_WIDTH;
+    }
+}
+
+void setupObstacles() {
+    // Trap year
+    if (year % 2 == 0)
+    {
+        traps[0] = (Rectangle){SCREEN_WIDTH*0.55, SCREEN_HEIGHT*0.3, 30, 30};
+        traps[1] = (Rectangle){SCREEN_WIDTH*0.3, SCREEN_HEIGHT*0.7, 30, 30};
+        traps[2] = (Rectangle){SCREEN_WIDTH*0.7, SCREEN_HEIGHT*0.6, 30, 30};
+
+        // Place lake outside the screen instead of removing it (to avoid extra if statements)
+        lake = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
+
+        numFoxes++;
+    }
+    // Lake year
+    else
+    {
+        lake = (Rectangle){SCREEN_WIDTH/2-50, SCREEN_HEIGHT/2-50, 100, 100};
+
+        // Place traps outside the screen instead of removing it (to avoid extra if statements)
+        traps[0] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
+        traps[1] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
+        traps[2] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
     }
 }
 
 void restart()
 {
-	bunny = (Rectangle){10, 490, 20, 20};
-	fox = (Rectangle){0, 0, 20, 20};
-	respawnFox();	
-	//trap = (Rectangle){SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2-100, 30, 30};
-	traps[0] = (Rectangle){SCREEN_WIDTH*0.55, SCREEN_HEIGHT*0.3, 30, 30};
-	traps[1] = (Rectangle){SCREEN_WIDTH*0.3, SCREEN_HEIGHT*0.7, 30, 30};
-	traps[2] = (Rectangle){SCREEN_WIDTH*0.7, SCREEN_HEIGHT*0.6, 30, 30};
-	// by default lake is not in the game (invisible = off screen), it appears in year 1
-	lake = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
+	bunny = (Rectangle){SCREEN_WIDTH/2-10, SCREEN_HEIGHT/2-10, 20, 20};
+	foxes[0] = (Rectangle){0, 0, 20, 20};
+	foxes[1] = (Rectangle){0, 0, 20, 20};
+	foxes[2] = (Rectangle){0, 0, 20, 20};
+	foxes[3] = (Rectangle){0, 0, 20, 20};
+	foxes[4] = (Rectangle){0, 0, 20, 20};
+	respawnFox(0);
+	respawnFox(1);
+	respawnFox(2);
+	respawnFox(3);
+	respawnFox(2);
+    numFoxes = 0;
+    setupObstacles();
 	gameOver = false;
     score = 0;
 	seasonX = SCREEN_WIDTH;
@@ -66,8 +91,6 @@ void restart()
 	isWinter = true;
 	isBunnyWinter = true;
 	year = 0;
-	areTrapsInYear = true;
-	areLakesInYear = false;
 	health = 300;
 }
 
@@ -88,6 +111,11 @@ float scaleSpeedWhenInLake(Rectangle r) {
     return 1.0;
 }
 
+void drawTextCentered(char* text, int x, int y, int fontSize, Color color) {
+    int width = MeasureText(text, fontSize);
+    DrawText(text, x-width/2, y, fontSize, color);
+}
+
 
 
 int main()
@@ -103,7 +131,7 @@ int main()
     Texture texWinter = LoadTextureFromImage(imWinter);
     UnloadImage(imWinter);
 
-    Image imSummer = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, (Color){ 0, 255, 0, 255 });
+    Image imSummer = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, (Color){ 97, 173, 53, 255 });
     Texture texSummer = LoadTextureFromImage(imSummer);
     UnloadImage(imSummer);
 
@@ -129,41 +157,26 @@ int main()
 				isBunnyWinter = isWinter;
 			}
 
-            bool foxWasSwimming = CheckCollisionRecs(fox, lake) && !isRectangleInWinter(lake);
+            bool lakeWasLiquid = !isRectangleInWinter(lake);
 			if (counter > SEASON_DURATION)
 			{
 				// Advance the season
 				seasonX -= SEASON_CHANGE_SPEED;
-				if (seasonX <= 0)
+				if (seasonX <= -70)
 				{
 					counter = 0;
-					seasonX = SCREEN_WIDTH;
+					seasonX = SCREEN_WIDTH+70;
 					isWinter = !isWinter;
 					if (isWinter)
 					{
 						year++;
-						//if (year==10)
-						if (year == 1)
+						if (year == 10)
 						{
 							win = true;
 							gameOver = true;
 						}
-						if (year == 1)
-						{
-							areTrapsInYear = false;
-							areLakesInYear = true;
-						}
-						if (!areTrapsInYear)
-						{
-							// Place trap outside the screen instead of removing it (to avoid extra if statements)
-							traps[0] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
-							traps[1] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
-							traps[2] = (Rectangle){SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 0, 0};
-						}
-						if (areLakesInYear)
-						{
-							lake = (Rectangle){SCREEN_WIDTH/2-50, SCREEN_HEIGHT/2-50, 100, 100};
-						}
+
+                        setupObstacles();
 					}					
 				}							
 			}
@@ -187,49 +200,77 @@ int main()
 			bunny.x = Clamp(bunny.x, 0, SCREEN_WIDTH-bunny.width);
 			bunny.y = Clamp(bunny.y, 0, SCREEN_HEIGHT-bunny.height);
 
-            //Move the fox
-            // Calculate the vector from the fox to the bunny, normalize it and multiply by the fox speed
-            Vector2 bunnyVector = {bunny.x+bunny.width/2, bunny.y+bunny.height/2};
-            Vector2 foxVector = {fox.x+fox.width/2, fox.y+fox.height/2};
-            Vector2 foxDirection = Vector2Subtract(bunnyVector, foxVector);
-            Vector2 foxMovement = Vector2Scale(Vector2Normalize(foxDirection), FOX_SPEED * scaleSpeedWhenInLake(fox));
-            // Foxs moves only when the bunny is in the season of it's color
-            bool isBunnySafe = isBunnyWinter == isRectangleInWinter(bunny);
-            if (!isBunnySafe)
+			for (int i = 0; i < numFoxes; i++)
             {
-                fox.x += foxMovement.x;
-                fox.y += foxMovement.y;
-            }
-		
+                Rectangle* fox = &foxes[i];
 
-            // Collision detection
-            if (CheckCollisionRecs(bunny, fox))
-            {
-				health--;
-				if (health <= 0)
-				{
-					gameOver = true;
-				}
+                //Move the fox
+                // Calculate the vector from the fox to the bunny, normalize it and multiply by the fox speed
+                Vector2 bunnyVector = {bunny.x+bunny.width/2, bunny.y+bunny.height/2};
+                Vector2 foxVector = {fox->x+fox->width/2, fox->y+fox->height/2};
+                Vector2 foxDirection = Vector2Subtract(bunnyVector, foxVector);
+                Vector2 foxMovement = Vector2Scale(Vector2Normalize(foxDirection), FOX_SPEED * scaleSpeedWhenInLake(*fox));
+                // Foxs moves only when the bunny is in the season of it's color
+                bool isBunnySafe = isBunnyWinter == isRectangleInWinter(bunny);
+                if (!isBunnySafe)
+                {
+                    fox->x += foxMovement.x;
+                    fox->y += foxMovement.y;
+                }
             }
-			
+	
+	
+            // Collision detection
+
+            // Being attacked by foxes or by traps
+            bool beingAttacked = false;
+			for (int i = 0; i < numFoxes; i++)
+            {
+                Rectangle* fox = &foxes[i];
+                if (CheckCollisionRecs(bunny, *fox))
+                {
+                    beingAttacked = true;
+                }
+            }
 			for (int i = 0; i < 3; i++)
 			{
 				if (CheckCollisionRecs(bunny, traps[i]))
 				{
-					//gameOver = true;
+                    beingAttacked = true;
 				}
-				if (CheckCollisionRecs(fox, traps[i]))
-				{
-					score += 1;
-					respawnFox();
-				}
+            }
+            if (beingAttacked)
+            {
+                health--;
+                if (health <= 0)
+                {
+                    gameOver = true;
+                }
+            }
+
+            // Foxes touching traps
+			for (int i = 0; i < 3; i++)
+			{
+                for (int j = 0; j < numFoxes; j++)
+                {
+                    Rectangle* fox = &foxes[j];
+                    if (CheckCollisionRecs(*fox, traps[i]))
+                    {
+                        score += 1;
+                        respawnFox(j);
+                    }
+                }
 			}
 
-            // Fox freezes in lake
-            if (foxWasSwimming && CheckCollisionRecs(fox, lake) && isRectangleInWinter(lake))
+            // Foxes freeze in lake
+			for (int i = 0; i < numFoxes; i++)
             {
-                score += 1;
-                respawnFox();
+                Rectangle* fox = &foxes[i];
+                if (CheckCollisionRecs(*fox, lake) && lakeWasLiquid && isRectangleInWinter(lake))
+                {
+                    score += 1;
+                    respawnFox(i);
+                }
             }
 		}		
 
@@ -267,26 +308,32 @@ int main()
 			}			
 			DrawRectangleRec(lake, isRectangleInWinter(lake) ? WHITE : BLUE);
 			DrawRectangleRec(bunny, isBunnyWinter ? WHITE : BROWN);
-			DrawRectangleRec(fox, ORANGE);
+			for (int i = 0; i < numFoxes; i++)
+            {
+                DrawRectangleRec(foxes[i], ORANGE);
+            }
 
 			DrawLineEx((struct Vector2){seasonX,0}, (struct Vector2){seasonX,SCREEN_HEIGHT}, 4, BLACK);
 			
 			if (gameOver)
 			{
-				if (win)
-				{
-					DrawText("You died of old age.", 50, SCREEN_HEIGHT/2-50, 70, BLUE);
-					DrawText(TextFormat("You survived %i years", year), 50, SCREEN_HEIGHT/2+30, 35, BLUE);
-					DrawText(TextFormat("And killed %i foxes", score), 50, SCREEN_HEIGHT/2+65, 35, BLUE);
-				}
-				else
-				{
-					int gameOverWidth = MeasureText(GAME_OVER_TEXT, 70);
-					int restartWidth = MeasureText("Press R to restart", 50);
-					DrawText(GAME_OVER_TEXT, SCREEN_WIDTH/2-gameOverWidth/2, SCREEN_HEIGHT/2-50, 70, RED);
-					DrawText("Press R to restart", SCREEN_WIDTH/2-restartWidth/2, SCREEN_HEIGHT/2+30, 50, RED);
+                int textX = SCREEN_WIDTH/2;
+                int textY = 180;
+                Color textColor = RED;
 
-				}
+                if (win) {
+                    textColor = BLUE;
+                    drawTextCentered("You died of old age.", textX, textY, 70, textColor);
+                }
+                else {
+                    drawTextCentered("Game Over!", textX, textY, 70, textColor);
+                }
+                textY += 90;
+                drawTextCentered(TextFormat("You survived %i years", year), textX, textY, 35, textColor);
+                textY += 40;
+                drawTextCentered(TextFormat("And killed %i foxes", score), textX, textY, 35, textColor);
+                textY += 70;
+                drawTextCentered("Press R to restart", textX, textY, 50, textColor);
 				
 			}
 			else
@@ -302,3 +349,23 @@ int main()
 	return 0;
 }
 
+/*
+Introduction
+(Press S to skip)
+Bunny (that's you)
+Fox (your enemy)
+[You die to the fox]
+[You press R]
+Your fur changes color with the seasons
+[2 season changes]
+[The fox spawns]
+The fox doesn't see you when you're hidden
+Lure the fox into traps
+[You lure the fox into a trap]
+[Traps disappear and lake appears]
+Lure the fox into the lake
+The fox will freeze with the lake in winter
+[You freeze the fox]
+[No fox spawns]
+Good luck! Press R to begin for REAL
+*/
